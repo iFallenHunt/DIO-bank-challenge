ca51ef54f7eac1c1bf67f5cb525382af3df64f1c
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-04-29 14:15:39 -0300

    Main class

--- src/TerminalAccount.java
+++ src/TerminalAccount.java
@@ -1,5 +1,12 @@
 public class TerminalAccount {
-    public static void main(String[] args) throws Exception {
-        
+    /**
+     * The main function that creates a BankAccount via terminal and displays the account creation message.
+     *
+     * @param  args	an array of strings containing command line arguments
+     */
+    
+    public static void main(String[] args) {
+        BankAccount account = BankAccount.createAccountViaTerminal();
+        account.displayAccountCreatedMessage();
     }
 }
